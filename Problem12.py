46a2f7ab84e7d05215a5bef2bcf04509fb7f27d0
Andrew Gillies <andrew@leastweasel.org>
2012-07-18 22:20:39 +0100

    Finally done Problem 12

--- Problem12.py
+++ Problem12.py
@@ -1,17 +1,40 @@
-from Euler import TriangularNumbers, PrimeFactorisor, CombinationsGenerator
+from Euler import TriangularNumbers, PrimeFactorisor
 
 def calculateNumberOfDivisors(n):
     pf = PrimeFactorisor(n)
     primeFactors = pf.getPrimeFactors()
     
-    cg = CombinationsGenerator(primeFactors)
+    factorCounts = {}
     
-    divisors = [1]
-    divisors.extend(cg.getUniqueCombinations())
-    
-    return divisors
+    for f in primeFactors:
+        if f in factorCounts:
+            factorCounts[f] = factorCounts[f] + 1
+        else:
+            factorCounts[f] = 1
 
-t = TriangularNumbers(10)
+    numDivisors = 0
+
+    for f in factorCounts:
+        # print "Factor count:", f, " = ", factorCounts[f]
+
+        if numDivisors == 0:
+            numDivisors = factorCounts[f] + 1
+        else:
+            numDivisors *= (factorCounts[f] + 1)
+            
+    return numDivisors
+
+t = TriangularNumbers(15000)
+maxDivisors = 0
 
 for n in t:
-    print calculateNumberOfDivisors(n)
+    numDivisors = calculateNumberOfDivisors(n)
+    #print "Triangular number",n,"has",numDivisors,"divisors" 
+
+    if numDivisors > maxDivisors:
+        maxDivisors = numDivisors
+        
+    if numDivisors > 500:
+        print "Triangular number",n,"has",numDivisors,"divisors" 
+        
+print "Maximum number of divisors = ",maxDivisors
